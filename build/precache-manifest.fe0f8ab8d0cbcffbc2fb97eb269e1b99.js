64bc286aa461f69518cddc772cf1512189db6d2f
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2019-09-30 13:56:10 +0800

    Createad build folder

---
+++ build/precache-manifest.fe0f8ab8d0cbcffbc2fb97eb269e1b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dbb1ef42678ad74a5c34d165c78865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1d7f1228ab74ffe33d0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9cf6ed858831d6ae94d8",
+    "url": "/static/js/2.ababd17c.chunk.js"
+  },
+  {
+    "revision": "b1d7f1228ab74ffe33d0",
+    "url": "/static/js/main.b3502749.chunk.js"
+  },
+  {
+    "revision": "200d255aba84118a229e",
+    "url": "/static/js/runtime-main.1d2ea64b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
